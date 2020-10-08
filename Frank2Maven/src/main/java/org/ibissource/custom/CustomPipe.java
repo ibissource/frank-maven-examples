64a0c954cc7bf7090aea9fcdb5af4284138933ac
Martijn Dirkse <martijn@integrationpartners.nl>
2020-10-08 09:21:28 +0200

    Test that attribute can do without getter

--- Frank2Maven/src/main/java/org/ibissource/custom/CustomPipe.java
+++ Frank2Maven/src/main/java/org/ibissource/custom/CustomPipe.java
@@ -7,18 +7,33 @@ import nl.nn.adapterframework.configuration.ConfigurationException;
 import nl.nn.adapterframework.stream.Message;
 import nl.nn.adapterframework.core.PipeRunException;
 import nl.nn.adapterframework.pipes.FixedForwardPipe;
+import nl.nn.adapterframework.doc.IbisDoc;
 
 import java.io.IOException;
 
-class CustomPipe extends FixedForwardPipe {
+public class CustomPipe extends FixedForwardPipe {
     private static final int ADDITION = 3;
+
+    private String customAttribute = "0";
+
+    @IbisDoc({"1", "This is a custom property"})
+    public void setCustomAttribute(String value) {
+        customAttribute = value;
+    }
+
+    /*
+    public String getCustomAttribute() {
+        return customAttribute;
+    }
+    */
 
     @Override
     public PipeRunResult doPipe(Message message, IPipeLineSession session) throws PipeRunException {
         try {
             String msg = message.asString();
             int val = Integer.parseInt(msg);
-            int result = val + ADDITION;
+            int attributeVal = Integer.parseInt(customAttribute);
+            int result = val + attributeVal + ADDITION;
             return new PipeRunResult(getForward(), (Object) Integer.valueOf(result));
         } catch(IOException e) {
             throw new PipeRunException(this, "An IOException occurred", e);
